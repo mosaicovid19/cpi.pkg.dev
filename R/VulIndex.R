2776845fa892ae18e4d23bc40e96bc0a3e486644
Felipe Figueiredo <philsf79@gmail.com>
2021-07-01 19:28:58 -0300

    base function

--- R/VulIndex.R
+++ R/VulIndex.R
@@ -106,21 +106,7 @@ VulIndex = function(basico,entorno,dom.i,dom.ii,pessoa,dom.renda,resp.alfa){
                                               ifelse(features.abs$V002/features.abs$V001p>89,0.2,0.1)))))
 
   # calcula a componente Entorno do IVC e aplica os pesos
-  compEntorno <-
-    #Logradouro
-    rowSums(features.abs[, c("V423", "V425", "V427")])/features.abs$V422 * (1/7) +
-    #IluminaÃ§Ã£o PÃºblica
-    rowSums(features.abs[, c("V429", "V431", "V433")])/features.abs$V422 * (1/7) +
-    #PavimentaÃ§Ã£o
-    rowSums(features.abs[, c("V435", "V437", "V439")])/features.abs$V422 * (1/7) +
-    #Meio-fio/guia
-    rowSums(features.abs[, c("V447", "V449", "V451")])/features.abs$V422 * (1/7) +
-    #Bueiro/Boca de lobo
-    rowSums(features.abs[, c("V453", "V455", "V457")])/features.abs$V422 * (1/7) +
-    #Esgoto
-    rowSums(features.abs[, c("V472", "V474", "V476")])/features.abs$V422 * (1/7) +
-    #Lixo
-    rowSums(features.abs[, c("V478", "V480", "V482")])/features.abs$V422 * (1/7)
+  compEntorno <- comp_entorno(features.abs)
 
   # Calcula componente de calculo mais de duas pessoas por domicÃ­lio
   # Calcula % de pessoas que moram sÃ³s ou com atÃ© mais uma outra pessoa
