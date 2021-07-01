d75d57e6c344321ac048d0a2e4e710698ba9f3c1
Felipe Figueiredo <philsf79@gmail.com>
2021-07-01 20:15:51 -0300

    whitespace reorganizado

--- R/comp_pessoas.R
+++ R/comp_pessoas.R
@@ -3,15 +3,12 @@ comp_pessoas <- function(data) {
   # calcula a componente Docmicilios com mulheres como mantenedoras e aplica os pesos
   compDomiciliosMulher <-
     # Qtd pessoas domicios com mulheres como mantenedoras / Qtd pessoas dos domicilios
-    (1-(rowSums(data[,
-                             c("V081","V082", "V083", "V084", "V085", "V086", "V087")]))/
+    (1-(rowSums(data[, c("V081","V082", "V083", "V084", "V085", "V086", "V087")]))/
        data$V422) * (1/3)
-
 
   # calcula % Pessoas brancas
   compPessoas <-
-    (1 - rowSums(data[,
-                              c("V003", "V004", "V005", "V006")])/data$V001p)*1/3
+    (1 - rowSums(data[, c("V003", "V004", "V005", "V006")])/data$V001p)*1/3
 
   # calcula % responsaveis analfa.
   compRespAlfa <-
