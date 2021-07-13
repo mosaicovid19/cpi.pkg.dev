be1df1c23e3b9d559a768194d2e6271cc6ba8876
Felipe Figueiredo <philsf79@gmail.com>
2021-07-13 12:48:00 -0300

    pesos movidos para o cálculo final do componente + indentação

--- R/comp_domicilio.R
+++ R/comp_domicilio.R
@@ -6,15 +6,15 @@ comp_domicilio <- function(data) {
   # 3 - Divide o resultado de 2 pelo nÃºmero total de pessoas vivendo com mais do que 2 pessoas
   D3M5 <-
     (1 - (rowSums(data[, c("V055", "V056", "V057", "V058", "V059")])/
-            (data$V422))) * (1/5)
+            (data$V422)))
 
   # Calcula % de pessoas com acesso a banheiro de uso exclusivo
   D1BAN <-
-    rowSums(data[,c("V016")]/data$V001) * (1/5)
+    rowSums(data[,c("V016")]/data$V001)
 
   # Calcula % de pessoas com acesso a rede de distribuiÃ§Ã£o de Ã¡gua
   D2AGU <-
-    rowSums(data[,c("V012")]/data$V001) * (1/5)
+    rowSums(data[,c("V012")]/data$V001)
 
   # Cria cluster por faixa de renda
   # calcula a componente Renda e aplica o peso
@@ -24,8 +24,11 @@ comp_domicilio <- function(data) {
                                        ifelse(data$V002DR/data$V001p>178,0.3,
                                               ifelse(data$V002DR/data$V001p>89,0.2,0.1)))))
 
-  D4REN <- D4REN * (2/5)
-  compDomicilios <- D2AGU + D1BAN + D3M5 + D4REN
+  compDomicilios <-
+    D2AGU * 1/5 +
+    D1BAN * 1/5 +
+    D3M5  * 1/5 +
+    D4REN * 2/5
 
   compDomicilios
 }
--- R/comp_pessoas.R
+++ R/comp_pessoas.R
@@ -4,16 +4,19 @@ comp_pessoas <- function(data) {
   H2MR <-
     # Qtd pessoas domicios com mulheres como mantenedoras / Qtd pessoas dos domicilios
     (1-(rowSums(data[, c("V081","V082", "V083", "V084", "V085", "V086", "V087")]))/
-       data$V422) * (1/3)
+       data$V422)
 
   # calcula % Pessoas brancas
   H1NB <-
-    (1 - rowSums(data[, c("V003", "V004", "V005", "V006")])/data$V001p)*1/3
+    (1 - rowSums(data[, c("V003", "V004", "V005", "V006")])/data$V001p)
 
   # calcula % responsaveis analfa.
   H3RA <-
-    (1 - rowSums(data[,c("V093")]/data$V001r))*1/3
+    (1 - rowSums(data[,c("V093")]/data$V001r))
 
-  compPessoas <- H1NB + H2MR + H3RA
+  compPessoas <-
+    H1NB * 1/3 +
+    H2MR * 1/3 +
+    H3RA * 1/3
   compPessoas
 }
