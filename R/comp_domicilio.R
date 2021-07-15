5efb90496197cb184a50c3188f7845a992673752
Felipe Figueiredo <philsf79@gmail.com>
2021-07-15 17:39:41 -0300

    D4REN

--- R/comp_domicilio.R
+++ R/comp_domicilio.R
@@ -5,14 +5,7 @@ comp_domicilio <- function(data) {
 
   D3M5 <- D3M5(data)
 
-  # Cria cluster por faixa de renda
-  # calcula a componente Renda e aplica o peso
-  # base DomicilioRenda
-  D4REN <- ifelse(data$V002DR/data$V001p>2090,1,
-                         ifelse(data$V002DR/data$V001p>1045,0.7,
-                                ifelse(data$V002DR/data$V001p>522.5,0.5,
-                                       ifelse(data$V002DR/data$V001p>178,0.3,
-                                              ifelse(data$V002DR/data$V001p>89,0.2,0.1)))))
+  D4REN <- D4REN(data)
 
   compDomicilios <-
     D2AGU * 1/5 +
