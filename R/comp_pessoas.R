29a2a98b187b3880cad1da7331f8dd1a03b6364a
Felipe Figueiredo <philsf79@gmail.com>
2021-07-15 17:41:52 -0300

    H2MR

--- R/comp_pessoas.R
+++ R/comp_pessoas.R
@@ -2,12 +2,7 @@ comp_pessoas <- function(data) {
 
   H1NB <- H1NB(data)
 
-  # calcula a componente Docmicilios com mulheres como mantenedoras e aplica os pesos
-  # base Domicilio01
-  H2MR <-
-    # Qtd pessoas domicios com mulheres como mantenedoras / Qtd pessoas dos domicilios
-    (1-(rowSums(data[, c("V081","V082", "V083", "V084", "V085", "V086", "V087")]))/
-       data$V422)
+  H2MR <- H2MR(data)
 
   # calcula % responsaveis analfa.
   # base Responsavel02
