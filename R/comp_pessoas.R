156c37499418975c216d89d8cb946bf028f325fd
Felipe Figueiredo <philsf79@gmail.com>
2021-07-21 22:25:17 -0300

    Variáveis e componente Humano convertidos para filtro

--- R/comp_pessoas.R
+++ R/comp_pessoas.R
@@ -1,14 +1,16 @@
 comp_pessoas <- function(data, group = Cod_setor) {
-
-  H1NB <- H1NB(data, group = {{group}})
-
-  H2MR <- H2MR(data, group = {{group}})
-
-  H3RA <- H3RA(data, group = {{group}})
-
-  compPessoas <-
-    H1NB * 1/3 +
-    H2MR * 1/3 +
-    H3RA * 1/3
-  compPessoas
+  data %>%
+    # acrescenta variável H1NB
+    H1NB(group = {{group}}) %>%
+    # acrescenta variável H2MR
+    H2MR(group = {{group}}) %>%
+    # acrescenta variável H3RA
+    H3RA(group = {{group}}) %>%
+    # acrescenta componente humano
+    mutate(
+      compPessoas =
+        H1NB * 1/3 +
+        H2MR * 1/3 +
+        H3RA * 1/3
+    )
 }
