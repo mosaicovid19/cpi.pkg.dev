274b2ab39e46665c37f7b80a5a92354040927074
Felipe Figueiredo <philsf79@gmail.com>
2021-08-01 05:39:52 -0300

    coletar o data.frame de trabalho antes dos cálculos

--- R/VulIndex.R
+++ R/VulIndex.R
@@ -4,6 +4,13 @@ VulIndex <- function(Basico = Basico, Domicilio01 = Domicilio01, Domicilio02 = D
 
   resumo <- base_preparo(Basico, Domicilio01, Domicilio02, DomicilioRenda, Entorno03, Pessoa03, Responsavel02)
 
+  ## SQLite 3.33.0 não aguenta esse tranco todo
+  ## Precisamos parar por aqui e coletar o resultado do preparo da base
+  if ("tbl_SQLite" %in% class(resumo)) {
+    resumo <- resumo %>%
+      collect()
+  }
+
 # calculo componentes -----------------------------------------------------
 
     resumo %>%
