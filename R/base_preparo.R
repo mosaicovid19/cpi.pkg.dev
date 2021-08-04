ef10e2e601fa676929200b7707aee6349f53ae73
Felipe Figueiredo <philsf79@gmail.com>
2021-08-04 15:56:16 -0300

    checar argumentos pelas classes conexões podem ser expandidas
    Com a indentação e a `vec_c`, agora podemos expandir no futuro para
    novas conexões apenas acrescentando novas classes (como Postgres) no `conexoes`

--- R/base_preparo.R
+++ R/base_preparo.R
@@ -1,13 +1,18 @@
 base_preparo <- function(Basico = NULL, Domicilio01 = NULL, Domicilio02 = NULL, DomicilioRenda = NULL, Entorno03 = NULL, Pessoa03 = NULL, Responsavel02 = NULL, con = NULL){
 
+  # conexões DB identificadas
+  conexoes <- vctrs::vec_c(
+    "SQLite",
+  )
+
   ## caso o usuário passe uma conexão, usar SQL e retornar
   if(!missing(con) &&
      !is.null(con) &&
-     dbIsValid(con)
+     class(con) %in% conexoes
      ) return( base_preparo_SQL(con) )
   ## se o usuário passar apenas um argumento, a conexão será o primeiro (Basico)
   if(!missing(Basico) &&
-     dbIsValid(Basico)
+     class(Basico) %in% conexoes
      ) return( base_preparo_SQL(Basico) )
 
   ## caso contrário, usar as bases individuais
